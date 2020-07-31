e272a9d475c5da4e1241358dea2ce3174d6bcfe5
Roshan Jacob <roshan.jacob2021@gmail.com>
2020-07-31 09:09:18 -0500

    delta, decimals, currency example

--- src/test/java/TestExample.java
+++ src/test/java/TestExample.java
@@ -1,4 +1,3 @@
-
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -11,6 +10,19 @@ public class TestExample {
         String company = "Codeup";
         assertEquals("Codeup", company);
 //        assertEquals("codeup", company);
-
     }
+
+    //When using floating values, Delta is a 3rd parameter required representing a positive margin of error
+    // of decimals
+
+    @Test
+    public void testIfChangeIsCorrect(){
+        Double price = 10.0;
+        Double discount = 4.5;
+
+        assertEquals(5.5, price - discount, 0);
+        assertEquals(5.1, price - discount, 0.5);
+        assertNotEquals(4.9, price -discount, 0.5);
+    }
+
 }
