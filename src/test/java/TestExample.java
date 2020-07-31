c489c179ac4138a43107034744947a8fc3367d3f
Roshan Jacob <roshan.jacob2021@gmail.com>
2020-07-31 10:17:19 -0500

    assertNotSame dealing with objects

--- src/test/java/TestExample.java
+++ src/test/java/TestExample.java
@@ -5,16 +5,15 @@ public class TestExample {
     //Make assertions that the expected value matches the actual value
     @Test
     public void myFirstTest(){
-//assertEquals: compare 2 values that are exactly the same
-//              assertion verifies that the expected value and the actual value are equal
+    //assertEquals: compare 2 values that are exactly the same
+    //assertion verifies that the expected value and the actual value are equal
         String company = "Codeup";
         assertEquals("Codeup", company);
-//        assertEquals("codeup", company);
+        //assertEquals("codeup", company);
     }
 
     //When using floating values, Delta is a 3rd parameter required representing a positive margin of error
     // of decimals
-
     @Test
     public void testIfChangeIsCorrect(){
         Double price = 10.0;
@@ -25,4 +24,23 @@ public class TestExample {
         assertNotEquals(4.9, price -discount, 0.5);
     }
 
+    //assertNotSame to verify 2 variables don't refer to the same object
+    //assertSame to verify 2 variables refer to the same object
+
+    @Test
+    public void testIfObjectsAreDifferent(){
+        //dog class and sheep class
+        Object dog = new Object();
+        Object sheep = new Object();
+
+        System.out.println("dog.toString() = " + dog.toString());
+        System.out.println("sheep.toString() = " + sheep.toString());
+
+        Object clonnedSheep = sheep;
+
+        System.out.println("clonnedSheep.toString = " + clonnedSheep.toString());
+
+        assertNotSame(sheep, dog);
+        assertSame(sheep, clonnedSheep);
+    }
 }
